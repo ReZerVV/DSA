539b7536eb5af6523f194060f4671862a5b90a4c
ReZerVV <kirillmorozov547@gmail.com>
2023-05-03 19:52:35 +0300

    Created task_b for graph traverse

--- graph_traversal/task_b/main.cpp
+++ graph_traversal/task_b/main.cpp
@@ -7,53 +7,73 @@
 */
 
 #include <iostream>
+#include <iomanip>
 #include "graph.hpp"
 #include <vector>
+#include <limits>
 
-void dfs(const graph &graph, std::vector<int32_t> &vertices, const int32_t start = 0) {
+void dfs(const graph &graph, const int32_t start = 0) {
     std::vector<bool> visitor( graph._adjacency_matrix.size(), false );
-    visitor[start] = true;
     
     std::stack<int32_t> stack_vertices{  };
     stack_vertices.push(start);
     
-    size_t vertex_counter = 0;
+    std::vector<int32_t> vertices( graph._adjacency_matrix.size(), std::numeric_limits<int32_t>::max() );
+    size_t step_counter = 0;
+    size_t edge_counter = 0;
+
     while (!stack_vertices.empty()) {
-        
+
         int32_t vertex = stack_vertices.top();
         stack_vertices.pop();
-        
+        if (visitor[vertex]) {
+            continue;
+        }
         visitor[vertex] = true;
-
-        vertices[vertex] = vertex_counter;
-        vertex_counter += 1;
         
+        step_counter += 1;
+        vertices[vertex] = step_counter;
+
+
         for (size_t u = 0; u < graph._adjacency_matrix[vertex].size(); ++u) {
             if (graph._adjacency_matrix[vertex][u] != NONE && !visitor[u]) {
                 stack_vertices.push(u);
+                edge_counter += 1;
             }
         }
     }
+
+    double graph_density = (edge_counter * 2.0d) / (graph._adjacency_matrix.size() * (graph._adjacency_matrix.size() - 1.0d));
+
+    std::cout << "INFO:" << std::endl;
+    std::cout << "\tvertex: " << graph._adjacency_matrix.size() << std::endl;
+    std::cout << "\tedge: " << edge_counter << std::endl;
+    std::cout << "graphic density: " << std::fixed << graph_density << std::endl;
+
+    std::cout << "\tvertices: " << std::endl;
+    for (size_t i = 0; i < vertices.size(); ++i) {
+        std::cout << i << "("<< vertices[i] << ") "; 
+    }
+    std::cout << std::endl;
 }
 
 int main(int argc, char** argv) {
     graph g{ 4 };
-    g.append_edge(0,3,1);
+    
     g.append_edge(0,1,1);
+    g.append_edge(0,3,1);
+
     g.append_edge(1,0,1);
     g.append_edge(1,2,1);
+
     g.append_edge(2,1,1);
+    g.append_edge(2,3,1);
+
     g.append_edge(3,2,1);
+    g.append_edge(3,0,1);
+
     std::cout << g << std::endl;
-
-    std::vector<int32_t> vs{ 0, 0, 0, 0};
     
-    dfs(g, vs);
-
-    for (size_t i = 0; i < vs.size(); ++i) {
-        std::cout << i << "(" << vs[i] << ") ";
-    }
-    std::cout << std::endl;
-
+    dfs(g);
     return 0;
 }
